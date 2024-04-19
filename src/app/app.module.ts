38cbefbb96caee1aef468edc5ce0648dc4525e15
Rener Sullivan <renersullivan1@gmail.com>
2024-04-19 13:48:02 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PerfilComponent,
+
+    HomeComponent
   ],
   imports: [
     BrowserModule,
