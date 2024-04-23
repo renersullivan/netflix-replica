9e27c94fc331c92bc8772dc45e84783e35c623b5
Rener Sullivan <renersullivan1@gmail.com>
2024-04-23 19:04:12 -0300

    criando page perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  {    path:'',component:HomeComponent  }
+  {    path:'',component:HomeComponent  },
+  { path:'perfil',component:PerfilComponent},
 
 
 ];
