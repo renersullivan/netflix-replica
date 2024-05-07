4bc0f8b5920719cd260e6f937431e81175e63f9a
Rener Sullivan <renersullivan1@gmail.com>
2024-05-07 13:26:24 -0300

    criando post responde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private http: HttpClient) { }
+
+  logar() {
+    const data = {
+      login: this.username,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:3000/login', data)
+      .subscribe(
+        (response) => {
+          console.log('Resposta do servidor:', response);
+          // Aqui você pode lidar com a resposta do servidor, como redirecionar para outra página se o login for bem-sucedido
+        },
+        (error) => {
+          console.error('Erro ao fazer login:', error);
+          // Aqui você pode lidar com o erro, como exibir uma mensagem de erro para o usuário
+        }
+      );
+  }
 }
